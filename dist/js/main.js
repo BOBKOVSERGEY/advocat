be2c3a3a124e4d3c82967fc3b95471f4c35284f6
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-09-06 14:01:31 +0300

    ready form

--- dist/js/main.js
+++ dist/js/main.js
@@ -462,6 +462,45 @@ $(function () {
   }
 
 
+  /*******inputfile******/
+
+  var inputs = document.querySelectorAll( '.inputfile' );
+  Array.prototype.forEach.call( inputs, function( input )
+  {
+    var label	 = input.nextElementSibling,
+      labelVal = label.innerHTML;
+
+    input.addEventListener( 'change', function( e )
+    {
+      var fileName = '';
+      if( this.files && this.files.length > 1 )
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+      else
+        fileName = e.target.value.split( '\\' ).pop();
+
+      if( fileName )
+        label.querySelector( 'span' ).innerHTML = fileName;
+      else
+        label.innerHTML = labelVal;
+    });
+  });
+
+  /*******end inputfile******/
+
+  /*** policy ***/
+  $('.js-policy').on('click', function($el){
+    checkPolicy();
+  });
+  /*** end policy ***/
+  function checkPolicy () {
+    if ($(".js-policy").is(':checked')) {
+      $("[type=submit]").prop('disabled', false);
+    } else {
+      $("[type=submit]").prop('disabled', true);
+    }
+  };
+
+
 
 
 });
