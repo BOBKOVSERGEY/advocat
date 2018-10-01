dca30c9e48c295f41f5f41524936361c93d7788d
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-10-01 17:05:51 +0300

    Created map

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,6 +18,7 @@ var userAgent = navigator.userAgent.toLowerCase(),
     counter: $(".counter"),
     owl: $(".owl-carousel"),
     selectFilter: $("select"),
+    googleMap: $('#google-map'),
   };
 
 $(function () {
@@ -508,6 +509,30 @@ $(function () {
       $("[type=submit]").prop('disabled', true);
     }
   };
+
+  if (plugins.googleMap.length) {
+    plugins.googleMap.googleMap({
+      styles: [{
+        "featureType": "landscape",
+        "stylers": [{"hue": "#FFBB00"}, {"saturation": 43.400000000000006}, {"lightness": 37.599999999999994}, {"gamma": 1}]
+      }, {
+        "featureType": "road.highway",
+        "stylers": [{"hue": "#FFC200"}, {"saturation": -61.8}, {"lightness": 45.599999999999994}, {"gamma": 1}]
+      }, {
+        "featureType": "road.arterial",
+        "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 51.19999999999999}, {"gamma": 1}]
+      }, {
+        "featureType": "road.local",
+        "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 52}, {"gamma": 1}]
+      }, {
+        "featureType": "water",
+        "stylers": [{"hue": "#0078FF"}, {"saturation": -13.200000000000003}, {"lightness": 2.4000000000000057}, {"gamma": 1}]
+      }, {
+        "featureType": "poi",
+        "stylers": [{"hue": "#00FF6A"}, {"saturation": -1.0989010989011234}, {"lightness": 11.200000000000017}, {"gamma": 1}]
+      }]
+    });
+  }
 
 
 
